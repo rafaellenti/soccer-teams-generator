a575657181073062239665157df23d7581bf4014
Rafael Lenti <rafael.lenti@gmail.com>
2023-09-19 11:30:40 -0300

    update code

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,25 +1,136 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import "./App.css";
+import PlayersInput from "./components/PlayersInput";
 
 function App() {
+  const [data, setData] = useState('');
+  const request: any = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify([
+      {
+        "name": "Rafael",
+        "stars": 2,
+        "position": "D"
+      },
+      {
+        "name": "Borga",
+        "stars": 3,
+        "position": "D"
+      },
+      {
+        "name": "Queiroz",
+        "stars": 3,
+        "position": "D"
+      },
+      {
+        "name": "Pedro",
+        "stars": 4,
+        "position": "M"
+      },
+      {
+        "name": "Lucas",
+        "stars": 5,
+        "position": "A"
+      },
+      {
+        "name": "Pardini",
+        "stars": 3,
+        "position": "A"
+      },
+      {
+        "name": "Machado",
+        "stars": 2,
+        "position": "A"
+      },
+      {
+        "name": "Hardy",
+        "stars": 3,
+        "position": "A"
+      },
+      {
+        "name": "Scarano",
+        "stars": 3,
+        "position": "A"
+      },
+      {
+        "name": "Ramon",
+        "stars": 3,
+        "position": "D"
+      },
+      {
+        "name": "Arara",
+        "stars": 3,
+        "position": "D"
+      },
+      {
+        "name": "Léo",
+        "stars": 3,
+        "position": "A"
+      },
+      {
+        "name": "Vicente",
+        "stars": 4,
+        "position": "D"
+      },
+      {
+        "name": "Arana",
+        "stars": 4,
+        "position": "M"
+      },
+      {
+        "name": "Castilho",
+        "stars": 6,
+        "position": "M"
+      },
+      {
+        "name": "Lukinha",
+        "stars": 3,
+        "position": "M"
+      },
+      {
+        "name": "Chula",
+        "stars": 6,
+        "position": "M"
+      },
+      {
+        "name": "Walter",
+        "stars": 2,
+        "position": "A"
+      }
+    ])
+  }
+
+  const generateTeams: any = async () => {
+    try {
+      const response: any = await fetch('http://localhost:8080/teams', request);
+      const data: any = await response.json();
+
+      setData(data);
+    }
+    catch (error: any) {
+      console.log(error.message);
+    }
+
+    console.log(data);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-md-8">
+            <PlayersInput />
+            <div className="text-center">
+              <button onClick={generateTeams} type="button" className="btn btn-primary mt-3">Gerar times</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
